8b4b789df32f051ddc0754a5394b219035abd55b
Nolo0329 <104756213+Nolo0329@users.noreply.github.com>
2022-09-09 02:20:01 +0200

    commits

--- exec_monty.c
+++ exec_monty.c
@@ -102,7 +102,7 @@ int run_monty(FILE *script_fd)
 {
 	stack_t *stack = NULL;
 	char *line = NULL;
-	size_t len = 0, exit_status = EXIT_SUCCESS;
+	size_t exit_status = EXIT_SUCCESS;
 	unsigned int line_number = 0, prev_tok_len = 0;
 	void (*op_func)(stack_t**, unsigned int);
 
